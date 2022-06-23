44b26392eaf2ed3a93c1d441ee0a304379614aa9
shaunvan119 <shaunvan@live.com.au>
2022-06-24 09:53:18 +1000

    tablet layout home page

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -5,7 +5,7 @@ function BottomSection() {
     return (
 
         <div className="main-bottom">
-            <img src={implement} alt="Logo" className="tesla" />
+            <img src={implement} alt="implement" className="implement" />
             <div className="bottom-right-p">
                 <h2 className="bottom-heading">Easy to implement</h2>
                 <p className="bottom-p">Our API comes with just a few lines of code. You’ll be up and<br/> 
--- src/components/MainTop.js
+++ src/components/MainTop.js
@@ -1,6 +1,6 @@
 import Contact from "./Contact"
 import phone from "../assets/home/desktop/illustration-phone-mockup.svg"
-
+import PhoneMobile from "../assets/home/desktop/phone-mobile.svg"
 
 
 function MainTop() {
@@ -14,8 +14,10 @@ function MainTop() {
                 <p><span className="questions">Have any questions?</span><span className="contact-us"> Contact Us</span></p>
                 <Contact/>
             
-                 <img src={phone} alt="Logo" className="phone" />
-            </div>
+                 <img src={phone} alt="Logo" className="phone" /> 
+                 <img src={PhoneMobile} alt="Logo" className="phoneMobile" /> 
+                          
+                 </div>
            
     )
 }
